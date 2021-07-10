4d9e95dae80fd3e8f23a42ff6fdd34bee0289f03
sebastian6740 <81164220+sebastian6740@users.noreply.github.com>
2021-07-10 01:45:15 -0400

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,7 @@
+from app.models import Componente, Fabricante, Producto
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Componente)
+admin.site.register(Fabricante)
+admin.site.register(Producto)
